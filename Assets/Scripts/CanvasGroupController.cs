4d539ebb81b6c994040bbbb4a83462ff7b9d2ea8
huge-glitch <2025000428@g.baewha.ac.kr>
2025-08-16 19:54:23 +0900

    계획 적어둠

--- Assets/Scripts/CanvasGroupController.cs
+++ Assets/Scripts/CanvasGroupController.cs
@@ -23,4 +23,10 @@ public class CanvasGroupController : MonoBehaviour
         cg.interactable = active;
         cg.blocksRaycasts = active;
     }
+
+    /*
+    Exit버튼은 기본적으로 비활성화 상태고 버튼 매니저 자식으로 들어감
+    만약 ShowCanvas호출 시 indexToShow가 0이 아니라면 Exit버튼 활성화, 왼오른버튼 비활성화
+    Exit버튼 누를 시 ShowCanvas(0); 실행하고 비활성화, 왼오른버튼 활성화
+    */
 }
