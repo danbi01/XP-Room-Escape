2a01d512bd5d53792af4ab86e5931da1b36c3071
huge-glitch <2025000428@g.baewha.ac.kr>
2025-09-24 14:50:27 +0900

    시작 전 설명 버튼 추가

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -4,25 +4,30 @@ using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.SceneManagement;
 public class StartButton : MonoBehaviour
 {
-    void Start()
+    public GameObject canvas;
+
+    public void StoryOkButtonClickHandler()
     {
-
-    }
-
-    void Update()
-    {
-
+        Debug.Log("StorySkip");
+        SceneManager.LoadScene("Intro");
     }
 
     public void StartButtonClickHandler()
     {
         Debug.Log("Start");
-        SceneManager.LoadScene("Story");
-    }
-
-    public void StoryOkButtonClickHandler()
-    {
-        Debug.Log("StorySkip");
         SceneManager.LoadScene("SouthWall");
     }
+
+    public void GameRuleOpen()
+    {
+        Debug.Log("GameRule");
+        canvas.transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    public void GameRuleClose()
+    {
+        Debug.Log("Close");
+        canvas.transform.GetChild(2).gameObject.SetActive(false);
+    }
+
 }
